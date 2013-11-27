9971a79771d9db3a07ef8f9575602d239b9798ad
Kun-Yi Li <lankudot@github.com>
2013-11-27 15:35:28 +0800

    build array list and new command /register

--- LoginSysteam/LoginSystem.java
+++ LoginSysteam/LoginSystem.java
@@ -8,7 +8,6 @@ public class LoginSystem
 	public static void main( String[] args )
 	{
 		Scanner keyboard = new Scanner( System.in );
-		UserManager uManager = new UserManager();
 		GetCommand gCommand = new GetCommand();
 
 		String userCommand;
@@ -16,28 +15,28 @@ public class LoginSystem
 		boolean go_on = true;
 
 		// Print the welcome message
-		System.out.println( "Login system Version 1.2" );
+		System.out.println( "Login system Version 1.3" );
 
+		// The loop will go on until user enter "/exit".
 		while ( go_on )
 		{
 			// Tell user that system is waiting for input.
 			System.out.print( "> " );
 			// Get user command from stdin
-			userCommand = keyboard.next();
+			userCommand = keyboard.nextLine();
 			// Get the CommandID
 			commandID = gCommand.getCommand( userCommand );
 
 			// Operating the command
 			switch ( commandID )
 			{
-			case 0:	// /exit
+			case 0:		// /exit
 				go_on = false;
 				break;
-			case 1:	// /register
-				System.out.println( "Register?" );
-				break;
-			case -1: //Command not found
+			case -1:	//Command not found
 				System.out.println( userCommand + ": Command not found!" );
+				break;
+			default:	//Command is handled by GetCommand
 				break;
 			}	// end switch( commandID )
 
